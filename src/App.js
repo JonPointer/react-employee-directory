a1366321eaf0e004180f3afb8734e9afd6070d74
Jon Pointer <jonpointer@msn.com>
2021-02-20 14:11:49 -0700

    Resolving branch structure and start of app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,57 @@
+import React, { Component } from "react";
+import EmployeeCard from "./components/EmployeeCard";
+// import Wrapper from "./components/Wrapper";
+// import Title from "./components/Title";
+// import friends from "./friends.json";
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    employees: {},
+  };
+
+  componentDidMount() {
+    this.loadEmployees("?inc=name,gender,dob,picture&results=25");
+  }
+
+  loadEmployees = query => {
+    API.search(query)
+      .then(res => this.setState({ employees: res.results }))
+      .catch(err => console.log(err));
+  };
+
+  removeEmployee = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const employees = this.state.employees.filter(employee => employee.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ employees });
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <div>
+        {
+          this.state.employees.map(employee => (
+            <FriendCard
+              removeEmployee={this.removeEmployee}
+              id={employee.id}
+              key={employee.id}
+              name={employee.name}
+              image={employee.image}
+              occupation={employee.occupation}
+              location={employee.location}
+            />
+          ))
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
 import logo from './logo.svg';
 import './App.css';
 
